27dd70e493cfb469d5213cdc490653533582c6b9
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2016-09-23 06:50:19 +0700

    Add weighted sum for all data examples

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+
 fn main() {
-    println!("Hello, world!");
+    // Define data
+    let x: Vec<[f32; 3]> = vec![
+        [1.0, 1.0, 1.0],
+        [0.0, 0.0, 1.0],
+        [1.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0],
+        [1.0, 1.0, 0.0]
+    ];
+
+    let bias: f32 = 1.0;
+    let weights: [f32; 4] = [0.6, 0.2, 0.4, 0.5];
+
+    // TODO: forward data to the perceptron
+    for xi in x.iter() {
+        let mut sum: f32 = 0.0;
+        for (index, value) in xi.iter().enumerate() {
+            let result: f32;
+            if index == 0 {
+                result = weights[index] * bias;
+            } else {
+                result = weights[index] * value;
+            }
+            sum += result;
+        }
+        println!("Weighted sum: {}", sum);
+        // TODO: run the activation function
+    }
+
+    // Weights
+    println!("Weights:");
+    for w in weights.iter() {
+        println!("{}", w);
+    }
 }
+
