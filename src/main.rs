d896ae6b185ee3718c5cdff8aa67759f8e855355
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2016-09-24 23:31:12 +0700

    Add logistic-sigmoid for the activation function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 
+// Logistic-sigmoid function
+fn activate_sigmoid(slope: f64, x: f64) -> f64 {
+    let e: f64 = std::f64::consts::E;
+    return 1.0/(1.0 + e.powf(-0.5 * slope * x));
+}
+
 fn main() {
     // Define data
-    let x: Vec<[f32; 3]> = vec![
+    let x: Vec<[f64; 3]> = vec![
         [1.0, 1.0, 1.0],
         [0.0, 0.0, 1.0],
         [1.0, 0.0, 0.0],
@@ -9,24 +15,32 @@ fn main() {
         [1.0, 1.0, 0.0]
     ];
 
-    let bias: f32 = 1.0;
-    let weights: [f32; 4] = [0.6, 0.2, 0.4, 0.5];
+    let bias: f64 = 1.0;
+    let weights: [f64; 4] = [0.6, 0.2, 0.4, 0.5];
 
     // TODO: forward data to the perceptron
     for xi in x.iter() {
-        let mut sum: f32 = 0.0;
+        let test: [f64; 4] = weights;
+        let mut sum: f64 = 0.0;
         for (index, value) in xi.iter().enumerate() {
-            let result: f32;
+            let result: f64;
             if index == 0 {
-                result = weights[index] * bias;
+                result = test[index] * bias;
             } else {
-                result = weights[index] * value;
+                result = test[index] * value;
             }
             sum += result;
         }
         println!("Weighted sum: {}", sum);
         // TODO: run the activation function
+        let result: f64 = activate_sigmoid(1.0, sum);
+        println!("Activation function: {}", result);
+
+        // TODO: Implement the perceptron learning algorithm
     }
+
+    // Bias
+    println!("Bias: {}", bias);
 
     // Weights
     println!("Weights:");
